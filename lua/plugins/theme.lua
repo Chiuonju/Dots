52b24b92654da46acd992615d328545d82a028ee
Chiuonju <chiuonju@gmail.com>
2025-04-15 19:00:03 +0700

    Change the color scheme and bold fonts

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,10 +1,56 @@
-return
-{
-    "folke/tokyonight.nvim",
-    lazy = false,
+return {
+    'catppuccin/nvim',
+    name = 'catppuccin',
     priority = 1000,
-    opts = {},
     config = function()
-        vim.cmd[[colorscheme tokyonight-storm]]
-    end
+        require('catppuccin').setup {
+            flavour = 'auto', -- latte, frappe, macchiato, mocha
+            background = {
+                light = 'latte',
+                dark = 'frappe',
+            },
+            transparent_background = false,
+            show_end_of_buffer = false,
+            term_colors = false,
+            dim_inactive = {
+                enabled = false,
+                shade = 'dark',
+                percentage = 0.15,
+            },
+            no_italic = false,
+            no_bold = false,
+            no_underline = false,
+            styles = {
+                comments = { 'italic' },
+                conditionals = { 'italic' },
+                loops = {},
+                functions = {},
+                keywords = {},
+                strings = {},
+                variables = {},
+                numbers = {},
+                booleans = {},
+                properties = {},
+                types = {},
+                operators = {},
+            },
+            color_overrides = {},
+            custom_highlights = {},
+            default_integrations = true,
+            integrations = {
+                cmp = true,
+                gitsigns = true,
+                nvimtree = true,
+                treesitter = true,
+                notify = false,
+                mini = {
+                    enabled = true,
+                    indentscope_color = '',
+                },
+            },
+        }
+
+        -- Set the colorscheme after setup
+        vim.cmd.colorscheme 'catppuccin'
+    end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -27,49 +27,49 @@ return {
     config = function(_, opts)
         require('nvim-treesitter.configs').setup(opts)
 
-        vim.defer_fn(function()
-            -- Treesitter highlight groups that typically have colors
-            local highlight_groups = {
-                '@keyword',
-                '@keyword.function',
-                '@keyword.operator',
-                '@keyword.return',
-                '@type',
-                '@type.definition',
-                '@type.builtin',
-                '@storageclass',
-                '@conditional',
-                '@repeat',
-                '@exception',
-                '@include',
-                '@namespace',
-                '@preproc',
-                '@define',
-                '@macro',
-                '@boolean',
-                '@constant',
-                '@function',
-                '@function.call',
-                '@method',
-                '@method.call',
-                '@field',
-                '@property',
-                '@parameter',
-                '@variable.builtin',
-                '@punctuation.special',
-                '@keyword.conditional',
-                '@string',
-                '@string.regex',
-                '@string.escape',
-            }
-
-            -- Apply bold+italic ONLY if the highlight group already has a color
-            for _, group in ipairs(highlight_groups) do
-                local hl = vim.api.nvim_get_hl(0, { name = group, link = false })
-                if hl and (hl.fg or hl.ctermfg) then -- Check if the group has a color
-                    vim.api.nvim_set_hl(0, group, { bold = true, fg = hl.fg })
-                end
-            end
-        end, 200) -- Delay to ensure Treesitter is fully loaded
+        -- vim.defer_fn(function()
+        --     -- Treesitter highlight groups that typically have colors
+        --     local highlight_groups = {
+        --         '@keyword',
+        --         '@keyword.function',
+        --         '@keyword.operator',
+        --         '@keyword.return',
+        --         '@type',
+        --         '@type.definition',
+        --         '@type.builtin',
+        --         '@storageclass',
+        --         '@conditional',
+        --         '@repeat',
+        --         '@exception',
+        --         '@include',
+        --         '@namespace',
+        --         '@preproc',
+        --         '@define',
+        --         '@macro',
+        --         '@boolean',
+        --         '@constant',
+        --         '@function',
+        --         '@function.call',
+        --         '@method',
+        --         '@method.call',
+        --         '@field',
+        --         '@property',
+        --         '@parameter',
+        --         '@variable.builtin',
+        --         '@punctuation.special',
+        --         '@keyword.conditional',
+        --         '@string',
+        --         '@string.regex',
+        --         '@string.escape',
+        --     }
+        --
+        --     -- Apply bold+italic ONLY if the highlight group already has a color
+        --     for _, group in ipairs(highlight_groups) do
+        --         local hl = vim.api.nvim_get_hl(0, { name = group, link = false })
+        --         if hl and (hl.fg or hl.ctermfg) then -- Check if the group has a color
+        --             vim.api.nvim_set_hl(0, group, { bold = true, fg = hl.fg })
+        --         end
+        --     end
+        -- end, 200) -- Delay to ensure Treesitter is fully loaded
     end,
 }
